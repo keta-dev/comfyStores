7e8defdbc7b8f99394bdfb75b011f25e71f0f059
keta-dev <89269904+keta-dev@users.noreply.github.com>
2022-12-21 14:22:10 +0100

    navbar UI setup

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import Home from './HomePage'
+import Products from './ProductsPage'
+import SingleProduct from './SingleProductPage'
+import About from './AboutPage'
+import Cart from './CartPage'
+import Error from './ErrorPage'
+import Checkout from './CheckoutPage'
+import Private from './PrivateRoute'
+
+// const index = () => {
+//   return (
+//     <div>index</div>
+//   )
+// }
+
+export { Home, Products, SingleProduct, About, Checkout, Private, Cart, Error }
